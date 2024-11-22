d407b89a31667337372761c6bbfd6722a9b115d2
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-11-22 13:11:49 +0800

    Design container for the grid

---
+++ index.js
@@ -0,0 +1,13 @@
+const GRID_MAXIMUM = 100;
+
+const createGrid = (num) => {
+  for(let i = 0; i < num; i++) {
+    const row = document.createElement('div')
+    for(let j = 0; j < num; j++) {
+      const col = document.createElement('div');
+    }
+  }
+}
+const calculateSquareHeight = (num) => {
+
+}
